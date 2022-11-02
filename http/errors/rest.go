2bb141548ac6cd05f117b21c3d192b1e8553b23f
halilylm <hyildirim@socialthinks.com>
2022-11-02 12:13:02 +0300

    implementing common http error package around microservices to normalize responses

---
+++ http/errors/rest.go
@@ -0,0 +1,86 @@
+package errors
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+const (
+	internalServerError = "internal server error"
+	notFoundError       = "request resource not found"
+	unauthorized        = "Unauthorized"
+)
+
+// httpError is the struct type of http errors
+type httpError struct {
+	Code     int   `json:"-"`
+	Message  any   `json:"message"`
+	Internal error `json:"-"`
+}
+
+// NewHTTPError generates new http error
+func NewHTTPError(code int, message any, internal error) error {
+	return &httpError{
+		Code:     code,
+		Message:  message,
+		Internal: internal,
+	}
+}
+
+// Error satisfies error interface.
+func (he *httpError) Error() string {
+	if he.Internal == nil {
+		return fmt.Sprintf("code=%d, message=%v", he.Code, he.Message)
+	}
+	return fmt.Sprintf("code=%d, message=%v, internal=%v", he.Code, he.Message, he.Internal)
+}
+
+// NewBadRequestError generates a new bad request http error
+func NewBadRequestError(message any, internal error) *httpError {
+	return &httpError{
+		Code:     http.StatusBadRequest,
+		Message:  message,
+		Internal: internal,
+	}
+}
+
+// NewInternalServerError generates a new internal server error
+func NewInternalServerError(internal error) *httpError {
+	return &httpError{
+		Code:     http.StatusInternalServerError,
+		Message:  internalServerError,
+		Internal: internal,
+	}
+}
+
+// NewNotFoundError generates not found error
+func NewNotFoundError(internal error) *httpError {
+	return &httpError{
+		Code:     http.StatusNotFound,
+		Message:  notFoundError,
+		Internal: internal,
+	}
+}
+
+// NewUnauthorizedError generates a new unauthorized error
+func NewUnauthorizedError(internal error) *httpError {
+	return &httpError{
+		Code:     http.StatusUnauthorized,
+		Message:  unauthorized,
+		Internal: internal,
+	}
+}
+
+// ParseErrorForResponse parses the http error from error
+func ParseErrorForResponse(err error) *httpError {
+	var httpErr *httpError
+	if errors.As(err, &httpErr) {
+		return &httpError{
+			Code:     httpErr.Code,
+			Message:  httpErr.Message,
+			Internal: httpErr.Internal,
+		}
+	}
+	return nil
+}
